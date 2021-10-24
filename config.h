0b1438b288662fabe44c5f9482749678fa92bbbe
Tung Beier <beiertu@mediamarktsaturn.com>
2021-10-24 21:10:23 +0200

    Configure externalpipe to open urls

--- config.h
+++ config.h
@@ -217,6 +217,8 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
+static char *openurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -o", "externalpipe", NULL };
+
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
@@ -235,6 +237,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_L,           externalpipe,   {.v = openurlcmd } },
 };
 
 /*

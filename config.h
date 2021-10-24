db0b76dfb2d848418f06ab95ee3f3d566046d2a9
Tung Beier <44575474+beiertu-mms@users.noreply.github.com>
2021-10-24 21:14:39 +0200

    Apply external pipe patch (#6)

--- config.h
+++ config.h
@@ -219,6 +219,8 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
+static char *openurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -o", "externalpipe", NULL };
+
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
@@ -237,6 +239,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_L,           externalpipe,   {.v = openurlcmd } },
 };
 
 /*

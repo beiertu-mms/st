68a42fca5b63ad904ded187713d32a3f9fb21ba6
Tung Beier <beiertu@mediamarktsaturn.com>
2022-05-01 20:32:28 +0200

    Adjust default fore- and background colors
    https://github.com/beiertu-mms/st/commit/9c9e15349afc0d235e0966c129dc092020188d33

--- config.h
+++ config.h
@@ -129,8 +129,8 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
